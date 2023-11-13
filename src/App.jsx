070c5c97f4bffa33d57da23dcbfc21fe67e28fd1
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-13 14:36:59 -0700

    added script inside the <div> to create a child <div> above the list of pups, displaying each individual puppys information.

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,21 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList)
   const [featPupId, setFeatPupId] = useState(null)
   console.log("puppyList:", puppyList);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
   return (
     <>
       <div>
-        <p>{featPupId}</p>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      
        {
         puppies.map((puppy) => {
           return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
