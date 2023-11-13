9b086a0a54ead8b9673d117dfce0e560722fbfe7
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-13 14:04:35 -0700

    applied key based on puppy.id; which is the same as the item number of the obejct within the puppies[], inside the data.js file.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import {useState} from  'react';
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies);
+  console.log("puppyList:", puppyList);
   return (
     <>
       <div>
        {
         puppies.map((puppy) => {
-          return <p>{puppy.name}</p>
+          return <p key={puppy.id}>{puppy.name}</p>
         })
        }
       </div>
